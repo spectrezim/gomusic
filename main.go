ee31b74c3cf690b3e9f0d4943de8ae8f744339ef
Benjamin Zimmerman <88516145+spectrezim@users.noreply.github.com>
2024-06-24 13:02:45 -0500

    Added basic support for environment variables

--- main.go
+++ main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
+func init() {
+	// Load environment variables
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
 func main() {
-	fmt.Println("initial commit")
+	test, exists := os.LookupEnv("TEST")
+	if exists {
+		fmt.Printf("Found TEST variable %s\n", test)
+	} else {
+		fmt.Println("No TEST variable found")
+	}
 }
